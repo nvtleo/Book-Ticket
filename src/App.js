9bb6e3181783dff102ac0bbf1c44393390c1a037
Trung <trungnguyen.02082000@gmail.com>
2023-11-04 16:37:23 +0700

    start prj

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// import logo from './logo.svg';
+import './App.css';
+import Main from './Project-movies/Main';
+
+
+
+
+
+function App() {
+  return (
+    <div className="App container">
+      <Main />
+    </div>
+  );
+}
+
+export default App;
